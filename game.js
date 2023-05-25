3bb6deb7acf0ab7e92472d16a9b976f9f3723e21
daskrys <caperezl@ucsc.edu>
2023-05-25 10:33:50 -0700

    added images to title screen

--- game.js
+++ game.js
@@ -7,13 +7,17 @@ class SceneOne extends Phaser.Scene
 
     preload ()
     {
-        
+        this.load.image('snail', 'assets/snail.jpg');
+        this.load.image('roly', 'assets/roly.png');
     }
 
     create () 
     {
         this.cameras.main.setBackgroundColor('#737373');
         this.add.text(90, 200, "Roly Poly: To the End", {color: '#FFFFFF'}).setFontSize(25);
+
+        this.snail = this.add.sprite(210, 300, 'snail').setScale(0.25);
+        this.poly = this.add.sprite(310, 500, 'roly').setScale(0.25);
     }
 
     update () 
